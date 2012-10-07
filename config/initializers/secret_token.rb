1b7b0312eca29ca6a43fc57f620de022b519a0b6
Ana Martinez <acemacu@gmail.com>
2012-10-07 15:05:47 -0700

    Project skeleton and database config

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BloggingForFun::Application.config.secret_token = 'c49d1b7ea2a5f5bc2888124a131c031e2e7759941f34880f354caa0ae85b1020b246700d22781415243a37d09768e4cd2ff83ab0b574426561851446335bb320'
